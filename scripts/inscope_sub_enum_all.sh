c8ae32a29eb5c4380ee4904fd6c6110bdf6f5ce1
Rachit-Codifier <44037733+rexj8@users.noreply.github.com>
2022-08-11 15:26:29 +0530

    Update inscope_sub_enum_all.sh

--- scripts/inscope_sub_enum_all.sh
+++ scripts/inscope_sub_enum_all.sh
@@ -29,6 +29,9 @@ sub_enum(){
 mkdir $2knockpy_report/
 sudo chmod 777 $2*
 
+amass enum -passive -d $1 -o $2amass_passive
+amass enum -active -d $1 -o $2amass_active
+amass enum -d $1 -o $2amass_normal
 afinder $1 $2assetfinder
 findomain $1 $2findomain
 subfindero $1 $2subfinder
